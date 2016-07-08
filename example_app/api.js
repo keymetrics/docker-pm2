c3ea7a7c52ff2f08dd46b01325780263aadd3d2a
Unitech <strzelewicz.alexandre@gmail.com>
2016-07-08 11:54:06 +0200

    add example app with API and Worker

---
+++ example_app/api.js
@@ -0,0 +1,10 @@
+
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+  console.log('Got incoming request');
+  res.writeHead(200);
+  res.end('hey');
+}).listen(process.env.PORT || 3000, function() {
+  console.log('App listening on port 3000');
+});
---
+++ example_app/worker.js
@@ -0,0 +1,4 @@
+
+setInterval(function() {
+  console.log('Worker doing his job');
+}, 2000);
