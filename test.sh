2ec0663744b9bf1e55de914b2ef8cfb068f7aa09
Unitech <strzelewicz.alexandre@gmail.com>
2016-07-31 12:34:37 -0700

    move docker pull into test file

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+docker pull keymetrics/pm2-docker-alpine
 docker run -d -p 5678:3000 -v `pwd`/example_app:/app -e "APP=ecosystem.yml" keymetrics/pm2-docker-alpine
 
 wget http://localhost:5678
