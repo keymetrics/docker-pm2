8ea3778ba8eb00bc71ae72824c14ae2f64fd92b7
Unitech <strzelewicz.alexandre@gmail.com>
2016-07-31 12:33:13 -0700

    edit test

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker run -p 5678:3000 -v `pwd`/example_app:/app -e "APP=ecosystem.yml" keymetrics/pm2-docker-alpine
+docker run -d -p 5678:3000 -v `pwd`/example_app:/app -e "APP=ecosystem.yml" keymetrics/pm2-docker-alpine
 
 wget http://localhost:5678
